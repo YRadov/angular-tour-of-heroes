66dee88ddcc16505300b876e04a5ffb879ba987f
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-08 11:50:31 +0300

    add some css

--- app/app.component.ts
+++ app/app.component.ts
@@ -70,7 +70,15 @@ const HEROES: Hero[] = [
       }
     `],
     template:`
+        <h1>{{title}}</h1>
         <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes"
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+        </ul>
         <div *ngIf="selectedHero">
           <h2>{{selectedHero.name}} details!</h2>
           <div><label>id: </label>{{selectedHero.id}}</div>
@@ -79,12 +87,7 @@ const HEROES: Hero[] = [
             <input [(ngModel)]="selectedHero.name" placeholder="name"/>
           </div>
         </div>
-        <ul class="heroes">
-          <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-          </li>
-        </ul>
-  `,
+        `,
 
 })
 export class AppComponent {
